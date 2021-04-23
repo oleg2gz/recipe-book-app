aec8d86c036fea3eb54c52bb1aeb529572608d20
Oleg Kravchenko <kitaist@kitaist.com>
2021-04-23 21:44:48 +0800

    Component/Create RecipeList & Recipe components

---
+++ src/RecipeList.js
@@ -0,0 +1,11 @@
+import Recipe from './Recipe'
+
+export default function RecipeList({ recipes }) {
+  return (
+    <div>
+      {recipes.map((recipe) => {
+        return <Recipe key={recipe.id} {...recipe} />
+      })}
+    </div>
+  )
+}
