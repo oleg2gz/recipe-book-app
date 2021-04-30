83fb4562687e59dea0ed375df2c7c78ff861d876
Oleg Kravchenko <kitaist@kitaist.com>
2021-04-30 16:45:56 +0800

    Feature/Add & Delete buttons functionality

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,15 @@
 import IngredientList from './IngredientList'
 
 export default function Recipe(props) {
-  const { name, cookTime, servings, instructions, ingredients } = props
+  const {
+    id,
+    name,
+    cookTime,
+    servings,
+    instructions,
+    ingredients,
+    handleRecipeDelete,
+  } = props
 
   return (
     <div className="recipe">
@@ -9,7 +17,14 @@ export default function Recipe(props) {
         <h3 className="recipe__title">{name}</h3>
         <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn--danger">Delete</button>
+          <button
+            onClick={() => {
+              handleRecipeDelete(id)
+            }}
+            className="btn btn--danger"
+          >
+            Delete
+          </button>
         </div>
       </div>
       <div className="recipe__row">
